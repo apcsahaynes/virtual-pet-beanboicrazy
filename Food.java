70062110499ae235fc3dca25336b5ccde8584a46
Gavin R. <105890278+beanboicrazy@users.noreply.github.com>
2026-03-12 10:41:08 -0500

    Final - Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,24 @@
-public class Food {
-    
+public class Food{
+    private String name;
+    private int energyIncrease;
+    private int happinessIncrease; 
+    private int weightGain;
+    public Food(String name, int energyIncrease, int happinessIncrease, int weightGain){
+        this.name = name;
+        this.energyIncrease = energyIncrease;
+        this.happinessIncrease=happinessIncrease;
+        this.weightGain=weightGain;
+    }
+    public int energyAmount() {
+        return energyIncrease;
+    }
+    public int weightAmount() {
+        return weightGain;
+    }
+    public int happinessAmount() {
+        return happinessIncrease;
+    }
+    public String getName() {
+      return name;
+  }
 }
