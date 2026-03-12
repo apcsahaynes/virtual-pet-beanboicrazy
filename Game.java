fc889739a6ab3e1b1083cc381b97f1257a28f4ea
Gavin R. <105890278+beanboicrazy@users.noreply.github.com>
2026-03-12 10:40:36 -0500

    Final - Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,19 @@
-public class Game {
-    
+public class Game{
+    private String name;
+    private int happinessIncrease; 
+    private int weightLosse;
+    public Game(String name,  int happinessIncrease, int weightLosse){
+        this.name = name;
+        this.happinessIncrease=happinessIncrease;
+        this.weightLosse=weightLosse;
+    }
+    public int weightAmount() {
+        return weightLosse;
+    }
+    public int happinessAmount() {
+        return happinessIncrease;
+    }
+    public String getName() {
+      return name;
+  }
 }
